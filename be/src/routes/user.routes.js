8ed19fa353afef2096fa0201ec902a0cf0231117
Owen Roth <rothowen27@gmail.com>
2023-04-03 10:45:35 -0600

    add swagger

--- be/src/routes/user.routes.js
+++ be/src/routes/user.routes.js
@@ -1,3 +1,62 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     User:
+ *       type: object
+ *       required:
+ *         - email
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the book
+ *         email:
+ *           type: string
+ *           description: users email
+ *         admin:
+ *           type: boolean
+ *           description: if the user is an admin
+ *         activities:
+ *            type: array
+ *            description: The activities the user has completed
+ *         createdAt:
+ *           type: string
+ *           format: date
+ *           description: The date the user was added
+ *       example:
+ *         id: d5fE_asz
+ *         email: veritext@gmail.com
+ *         admin: true
+ *         activities: [1, 2, 3]
+ *         createdAt: 2020-03-10T04:05:06.157Z
+ */
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: Users management and retrieval
+ * /users:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Users]
+ *     requestBody:
+ *       required: false
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: Users Retrieved.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Some server error
+ *
+ */
+
 import { Router } from 'express'
 import { UsersController } from '../controllers/users.controller.js'
 

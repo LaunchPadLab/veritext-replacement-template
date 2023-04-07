04c609c6f08667109a4f5713d3935311a5989525
Owen Roth <rothowen27@gmail.com>
2023-04-07 12:57:41 -0500

    update schema and migration files

--- be/prisma/migrations/20230407161322_init/migration.sql
+++ be/prisma/migrations/20230407161322_init/migration.sql
@@ -13,23 +13,27 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "Folder" (
-    "id" TEXT NOT NULL,
-    "parentFolderId" TEXT,
+    "id" SERIAL NOT NULL,
+    "parentFolderId" INTEGER,
     "repository" "Repository" NOT NULL,
     "name" TEXT NOT NULL,
     "path" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "File" (
-    "id" TEXT NOT NULL,
-    "parentFolderId" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
+    "folderId" INTEGER NOT NULL,
     "repository" "Repository" NOT NULL,
     "name" TEXT NOT NULL,
     "path" TEXT NOT NULL,
     "url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "File_pkey" PRIMARY KEY ("id")
 );
@@ -38,7 +42,7 @@ CREATE TABLE "File" (
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
-ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentFolderId_fkey" FOREIGN KEY ("parentFolderId") REFERENCES "Folder"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentFolderId_fkey" FOREIGN KEY ("parentFolderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "File" ADD CONSTRAINT "File_parentFolderId_fkey" FOREIGN KEY ("parentFolderId") REFERENCES "Folder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;

47931e7f283374beca0080cddea4fec0cbf862d6
Owen Roth <69156111+oroth8@users.noreply.github.com>
2023-04-10 13:18:04 -0500

    Update Schema and Include Seeds (#21)

---
+++ be/prisma/migrations/20230407181400_folder_file_tables/migration.sql
@@ -0,0 +1,35 @@
+-- CreateEnum
+CREATE TYPE "Repository" AS ENUM ('VAULT', 'EXHIBIT_SHARE');
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "parentFolderId" INTEGER,
+    "repository" "Repository" NOT NULL,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" SERIAL NOT NULL,
+    "folderId" INTEGER NOT NULL,
+    "repository" "Repository" NOT NULL,
+    "name" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Folder" ADD CONSTRAINT "Folder_parentFolderId_fkey" FOREIGN KEY ("parentFolderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ be/prisma/seeds/seeders/fileSeeder.js
@@ -0,0 +1,90 @@
+import prisma from '../prismaClient.js'
+
+const getSubFolders = async () => {
+  const subFolders = await prisma.folder.findMany({
+    where: {
+      parentFolderId: { not: null },
+    },
+    include: {
+      parentFolder: true,
+    },
+  })
+  return subFolders
+}
+
+const fileSeeder = async () => {
+  const subFolders = await getSubFolders()
+
+  console.log('seeding files')
+  subFolders.forEach(async (folder) => {
+    await prisma.file.createMany({
+      data: [
+        {
+          name: 'Evidence 1',
+          repository: 'VAULT',
+          url: 'https://veritext-status-pages.s3.amazonaws.com/replacement-stock-files/giammarco-boscaro-zeH-ljawHtg-unsplash.jpg',
+          path: `/folders/${folder.parentFolder.name}/${
+            folder.name
+          }/${encodeURIComponent('Evidence 1')}`,
+          folderId: folder.id,
+        },
+        {
+          name: 'Evidence 2',
+          repository: 'VAULT',
+          url: 'https://veritext-status-pages.s3.amazonaws.com/replacement-stock-files/mikhail-pavstyuk-EKy2OTRPXdw-unsplash.jpg',
+          path: `/folders/${folder.parentFolder.name}/${
+            folder.name
+          }/${encodeURIComponent('Evidence 2')}`,
+          folderId: folder.id,
+        },
+        {
+          name: 'Evidence 3',
+          repository: 'VAULT',
+          url: 'https://veritext-status-pages.s3.amazonaws.com/replacement-stock-files/tingey-injury-law-firm-veNb0DDegzE-unsplash.jpg',
+          path: `/folders/${folder.parentFolder.name}/${
+            folder.name
+          }/${encodeURIComponent('Evidence 3')}`,
+          folderId: folder.id,
+        },
+        {
+          name: 'Evidence 4',
+          repository: 'VAULT',
+          url: 'https://veritext-status-pages.s3.amazonaws.com/replacement-stock-files/tingey-injury-law-firm-yCdPU73kGSc-unsplash.jpg',
+          path: `/folders/${folder.parentFolder.name}/${
+            folder.name
+          }/${encodeURIComponent('Evidence 4')}`,
+          folderId: folder.id,
+        },
+        {
+          name: 'Video Evidence',
+          repository: 'VAULT',
+          url: 'https://veritext-status-pages.s3.amazonaws.com/replacement-stock-files/4c60f406-427c-4535-9e0a-8381669bea5d_1658790244+(1).mp4',
+          path: `/folders/${folder.parentFolder.name}/${
+            folder.name
+          }/${encodeURIComponent('Video Evidence')}`,
+          folderId: folder.id,
+        },
+        {
+          name: 'Court Transcript',
+          repository: 'VAULT',
+          url: 'https://veritext-status-pages.s3.amazonaws.com/replacement-stock-files/Project+proposal+(1).pdf',
+          path: `/folders/${folder.parentFolder.name}/${
+            folder.name
+          }/${encodeURIComponent('Court Transcript')}`,
+          folderId: folder.id,
+        },
+        {
+          name: 'Court Motion',
+          repository: 'VAULT',
+          url: 'https://veritext-status-pages.s3.amazonaws.com/replacement-stock-files/Project+proposal.pdf',
+          path: `/folders/${folder.parentFolder.name}/${
+            folder.name
+          }/${encodeURIComponent('Court Motion')}`,
+          folderId: folder.id,
+        },
+      ],
+    })
+  })
+}
+
+export default fileSeeder

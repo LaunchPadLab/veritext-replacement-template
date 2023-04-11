2b3d68a526a1442ac71f30e3f9e6ee2c7c65ce91
Travis Vander Hoop <vanderhoop@me.com>
2023-04-11 14:54:24 -0600

    Bugfix: ensure router is memoized rather than wiped on every access; ensure instance can reference path
      - Make FE request to /api/v1/health and append class to root
        - requires setting expected API base url env var that differs from other params, but that is expected by the front-end API client
      - accommodate successDataPath config that expects data to be nested at data.attributes in response
        - I initially used a smaller payload, but this successDataPath was resulting in empty responses, which was confusing
      - update e2e test to assert on presence of class on root element, as this is an actual end to end test
        - we can update the health check once the DB is completely setup, but this was the minimal amount of work that will protect us in CI

--- cypress/e2e/home/home.cy.js
+++ cypress/e2e/home/home.cy.js
@@ -4,7 +4,8 @@ describe('Home Page', () => {
   beforeEach(() => {
     cy.visit(URL)
   })
-  it('renders a main section', () => {
-    cy.get('main').should('exist')
+
+  it('renders a main section that successfully interacts with the API', () => {
+    cy.get('.backend-healthy main').should('exist')
   })
 })

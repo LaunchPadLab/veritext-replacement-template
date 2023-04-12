141263fcd566382767258cae229b0539a863b852
Travis Vander Hoop <vanderhoop@me.com>
2023-04-12 08:18:55 -0600

    Bugfix: ensure backend api routes are configured and respond to request (#30)

--- be/server.js
+++ be/server.js
@@ -1,10 +1,11 @@
 
 
 import { App } from './app.js';
+import { HealthRoute } from './src/routes/health.routes.js';
 import { UserRoute } from './src/routes/user.routes.js';
 
 // ValidateEnv();
 
-const app = new App([new UserRoute()]);
+const app = new App([new HealthRoute(), new UserRoute()]);
 
 app.listen();
